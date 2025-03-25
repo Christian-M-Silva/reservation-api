6367f61a095e8151bb5193ca19656300fc541a89
Christian Silva <tininjacms@gmail.com>
2025-03-25 17:54:11 -0300

    feat(baseEntity): Add a base entity

---
+++ ReservationApi/Models/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationApi.Models.DataBase
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? UpdateAt { get; set; }
+    }
+}
