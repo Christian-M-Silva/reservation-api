6972a1aed5f3601b95f68b6383b1aed8cb4ca5cf
Christian Silva <tininjacms@gmail.com>
2025-03-26 16:22:37 -0300

    feat(ReservationEntity): Criará o entite de ReservationEntity

---
+++ ReservationApi/Models/Entities/ReservationEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ReservationApi.Models.Entities
+{
+    public class ReservationEntity:BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateOnly CheckIn { get; set; }
+        public DateOnly CheckOut { get; set; }
+        public int RoomNumber { get; set; }
+        public Guid ClientId { get; set; }
+    }
+}
