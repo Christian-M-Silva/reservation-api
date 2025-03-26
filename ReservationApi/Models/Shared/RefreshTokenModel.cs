67ddd3a0f1ab9f1f8f11a35570e791fb2dc72152
Christian Silva <tininjacms@gmail.com>
2025-03-26 16:08:24 -0300

    feat(RefreshTokenModel): Criará o model de RefreshTokenModel

---
+++ ReservationApi/Models/Shared/RefreshTokenModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ReservationApi.Models.Shared
+{
+    public class RefreshTokenModel
+    {
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime ExpirationDateRefreshToken { get; set; }
+    }
+}
