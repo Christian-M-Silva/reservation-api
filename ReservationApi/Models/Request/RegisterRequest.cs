52ddf4874a683fca13f129dec9cc7bc73fa887cc
Christian Silva <tininjacms@gmail.com>
2025-03-26 15:56:19 -0300

    feat(RegisterRequest): Criará o model de request de register

---
+++ ReservationApi/Models/Request/RegisterRequest.cs
@@ -0,0 +1,10 @@
+﻿using ReservationApi.Models.Enuns;
+
+namespace ReservationApi.Models.Request
+{
+    public class RegisterRequest:LoginRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public RoleEnum Role { get; set; }
+    }
+}
