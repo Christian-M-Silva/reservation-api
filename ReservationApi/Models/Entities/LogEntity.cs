ecb6b0726f83294560609696b5c910fcf7940817
Christian Silva <tininjacms@gmail.com>
2025-03-26 16:32:36 -0300

    feat(LogAndReservationEntity): Criarpa o logEntity e add as annotations na Reservation

---
+++ ReservationApi/Models/Entities/LogEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationApi.Models.Entities
+{
+    public class LogEntity
+    {
+        [Required(ErrorMessage = "Request type is required")]
+        public string RequestType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Route is required")]
+        public string Route { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Log time is required")]
+        public DateTime LogTime { get; set; }
+    }
+}
--- ReservationApi/Models/Entities/ReservationEntity.cs
+++ ReservationApi/Models/Entities/ReservationEntity.cs
@@ -1,11 +1,23 @@
-﻿namespace ReservationApi.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationApi.Models.Entities
 {
     public class ReservationEntity:BaseEntity
     {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(255, ErrorMessage = "Name must have a maximum of 255 characters")]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Check-in date is required")]
         public DateOnly CheckIn { get; set; }
+
+        [Required(ErrorMessage = "Check-out date is required")]
         public DateOnly CheckOut { get; set; }
+
+        [Required(ErrorMessage = "Room number is required")]
         public int RoomNumber { get; set; }
+
+        [Required(ErrorMessage = "Client id is required")]
         public Guid ClientId { get; set; }
     }
 }
