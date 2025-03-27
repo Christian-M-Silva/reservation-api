b6aa394e8c19bcc5267bf8454394c7f39332a565
Christian Silva <tininjacms@gmail.com>
2025-03-27 18:06:34 -0300

    feat(auth): Criará controller e interface de auth implementando o método de registrar

---
+++ ReservationApi/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReservationApi.Interfaces.IServices;
+using ReservationApi.Models.Entities;
+using ReservationApi.Models.Enuns;
+using ReservationApi.Models.Request;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReservationApi.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuth AuthService) : ControllerBase
+    {
+        readonly IAuth _authService = AuthService;
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterRequest registerRequest)
+        {
+            try
+            {
+                if (!Enum.IsDefined(typeof(RoleEnum), registerRequest.Role))
+                {
+                    return BadRequest("Role Unacceptable");
+                }
+                UserEntity user = await _authService.RegisterAsync(registerRequest);
+                return Created("", new {email =  user.Email, name = user.Name});
+            }
+            catch (Exception ex) { 
+                return StatusCode(500, ex.Message);
+            }
+        }
+        // GET: api/<AuthController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AuthController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // PUT api/<AuthController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AuthController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ReservationApi/Interfaces/IServices/IAuth.cs
@@ -0,0 +1,10 @@
+﻿using ReservationApi.Models.Entities;
+using ReservationApi.Models.Request;
+
+namespace ReservationApi.Interfaces.IServices
+{
+    public interface IAuth
+    {
+        Task<UserEntity> RegisterAsync(RegisterRequest user);
+    }
+}
