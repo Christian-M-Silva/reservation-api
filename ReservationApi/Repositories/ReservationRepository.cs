c4d0b0ca7e472160a87ffa6e3562ce366f287dab
Christian Silva <tininjacms@gmail.com>
2025-04-08 12:33:30 -0300

    feat(repositoryGetReservation): Criará o reposrsitorio de pegar as reservas de forma dinamica

---
+++ ReservationApi/Repositories/ReservationRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReservationApi.Data;
+using ReservationApi.Interfaces.IRepositories;
+using ReservationApi.Models.Entities;
+using ReservationApi.Models.Request;
+
+namespace ReservationApi.Repositories
+{
+    public class ReservationRepository(MyDbContext dbContext) : IReservationRepository
+    {
+        private readonly MyDbContext _dbContext = dbContext;
+
+        public async Task<IEnumerable<ReservationEntity?>> GetReservations(FilterRequest filterRequest)
+        {
+            try
+            {
+                var query = _dbContext.Reservations.Where(coluna => coluna.ClientId == filterRequest.IdClient);
+
+                if (filterRequest.CheckIn.HasValue)
+                {
+                    query = query.Where(coluna => coluna.CheckIn == filterRequest.CheckIn);
+                }
+                if (filterRequest.RoomNumber.HasValue)
+                {
+                    query = query.Where(coluna => coluna.RoomNumber == filterRequest.RoomNumber);
+                }
+
+                return await query.ToListAsync();
+            }
+            catch (Exception err)
+            {
+                throw new Exception(err.Message);
+            }
+        }
+    }
+}
